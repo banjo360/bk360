044c3c83921ea8d0438ed014c5d4e03b1072461f
minirop <minirop@hotmail.com>
2023-10-11 15:16:01 +0200

    added hut animations array

--- src/mm_huts.c
+++ src/mm_huts.c
@@ -1,3 +1,10 @@
-#include "defines.h"
+#include "actor.h"
+#include "asset.h"
+
+const ActorAnimationInfo chhutAnimations[3] = {
+    {0, 0.0f}, 
+    {ASSET_4E_ANIM_MUDHUT_SMASH, 0.25f}, 
+    {ASSET_4E_ANIM_MUDHUT_SMASH, 1000000.0f}
+};
 
 const s32 mm_huts_data[] = { 0, 1, 2, 3, 6, 4 };
